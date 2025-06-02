11e909903b063927850d959e95b274e8920bfc3d
Pedro Bertochi <pedro.bertochi@nimofast.com>
2025-06-02 16:22:19 -0300

    Correcao do README

--- src/classificar.py
+++ src/classificar.py
@@ -1,30 +1,60 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[15]:
+
+
 import os
-from src.core import criar_modelo, combinar_imagens
-import numpy as np
 import shutil
 import tensorflow as tf
+from core import criar_modelo, combinar_imagens
+from utils import save_accuracy
+import numpy as np
 
-modelo = criar_modelo()
-train_gen = ParImageGenerator('../image/treino', batch_size=8)
-modelo.fit(train_gen, epochs=10)
-modelo.save('../model/modelo_final_media.h5')
 
-modelo = tf.keras.models.load_model('../model/modelo_final_media.h5')
+# In[16]:
+
+
+# modelo = tf.keras.models.load_model('../model/modelo_solda_resnet50.h5')
+modelo = tf.keras.models.load_model('../model/modelo_final_media_funcional.h5')
 caminho = '../image/classificacao/novas'
 imagens = sorted([f for f in os.listdir(caminho) if f.lower().endswith('.jpg')])
+
+os.makedirs('../results', exist_ok=True)
+
+
+# In[18]:
+
 
 if len(imagens) % 2 != 0:
     print("[AVISO] Número ímpar de imagens! A última será ignorada.")
     imagens = imagens[:-1]
 
 
+# In[19]:
+
+
 for i in range(0, len(imagens), 2):
     p1 = os.path.join(caminho, imagens[i])
     p2 = os.path.join(caminho, imagens[i+1])
+
+    # Preparar a entrada para o modelo
     entrada = combinar_imagens(p1, p2)
     entrada = np.expand_dims(entrada / 255.0, axis=0)
-    pred = modelo.predict(entrada)[0][0]
+
+    # Predição do modelo
+    pred = modelo.predict(entrada, verbose=0)[0][0]
+
     destino = 'boa' if pred >= 0.8 else 'ruim'
+    print(f"Predição: {pred:.2f} - Imagens: {imagens[i]} e {imagens[i+1]} - Destino: {destino}")
+
+
+    save_accuracy("../results/accuracy.txt", pred, imagens[i], imagens[i+1])
+
+    # Criar diretório de destino se não existir
     os.makedirs(f'../image/classificacao/{destino}', exist_ok=True)
-    shutil.move(p1, f'../image/classificacao/{destino}/{os.path.basename(p1)}')
-    shutil.move(p2, f'../image/classificacao/{destino}/{os.path.basename(p2)}')
+
+    # Mover as imagens para o diretório de destino
+    shutil.move(p1, os.path.join(f'../image/classificacao/{destino}', os.path.basename(p1)))
+    shutil.move(p2, os.path.join(f'../image/classificacao/{destino}', os.path.basename(p2)))
+
--- treinar_modelo.py
+++
@@ -1,7 +0,0 @@
-from src.core import criar_modelo
-from src.data import ParImageGenerator
-
-modelo = criar_modelo()
-train_gen = ParImageGenerator('../image/treino', batch_size=8)
-modelo.fit(train_gen, epochs=10)
-modelo.save('../model/modelo_solda_resnet50.h5')
